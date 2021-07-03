d277a593fe771a46289abf97ebd37fe1204dfc54
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2021-07-03 11:56:44 -0400

    add fuzzingutil

--- shell.nix
+++ shell.nix
@@ -1,30 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }:
-  with pkgs.buildPackages;
-  pkgs.mkShell {
-    nativeBuildInputs = 
-      [ cmake
-        clang_10
-        cargo
-        git
-        llvm_10
-        python2
-        # (python38.withPackages (ppkgs : with ppkgs; [pip]))
-        python
-        z3
-        zlib
-        linuxHeaders
-        ninja
-        which
-        lit
-        libffi
-        libuuid
-        curl
-        glibc
-        man
-      ];
-    shellHook = ''
-      export UCLIBC_KERNEL_HEADERS=${linuxHeaders}/include
-      export CLANG_INCLUDE_DIR=${clang_10.cc.lib}/lib/clang/${clang_10.version}/include
-    ''
-    ;
-}
+with import <nixpkgs> {};
+callPackage ./default.nix
+  { stdenv = llvmPackages_10.stdenv;
+    llvm = llvmPackages_10.llvm;
+  }
